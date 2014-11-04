c3c300c43796f8fcefab1c8acec045906611752a
Andrey Drebot <a.drebot@gmail.com>
2014-11-05 00:55:37 +0200

    Week 3 'after' files were added

---
+++ week3/after/js/scripts.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+
+    var viewModel = {
+        title: 'The Team',
+        personToAdd: personViewModel(),
+        people: ko.observableArray([]),
+        add: add,
+        remove: remove
+    };
+
+    function add() {
+        var obj = personViewModel(viewModel.personToAdd.firstname(), viewModel.personToAdd.lastname());
+        viewModel.people.push(obj);
+        viewModel.personToAdd.firstname('');
+        viewModel.personToAdd.lastname('');
+    }
+
+    function remove(obj) {
+        viewModel.people.remove(obj);
+    }
+
+    viewModel.people.push(personViewModel('Andrey', 'Drebot'));
+    viewModel.people.push(personViewModel('Ivan', 'Ivanov'));
+
+    function personViewModel(fn, ln) {
+        return {
+            firstname: ko.observable(fn),
+            lastname: ko.observable(ln)
+        };
+    }
+
+    setTimeout(function () {
+        viewModel.people.push(personViewModel('Petr', 'Petrov'));
+    }, 3000);
+
+    ko.applyBindings(viewModel);
+})
