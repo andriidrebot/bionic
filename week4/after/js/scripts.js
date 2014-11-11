e969a326da83c698a6ae0f92c6fcdc9ac02f1352
Andrey Drebot <a.drebot@gmail.com>
2014-11-11 19:00:23 +0200

    Week 4 part1

--- week4/after/js/scripts.js
+++ week4/after/js/scripts.js
@@ -1,5 +1,17 @@
 ﻿$(function () {
 
-    
-    
+    var viewModel = {
+        tasks: ko.observableArray([]),
+
+        taskToAdd: ko.observable(),
+        add: function () {
+            viewModel.tasks.push({
+                title: viewModel.taskToAdd()
+            });
+            viewModel.taskToAdd('');
+        }
+    }
+
+    ko.applyBindings(viewModel);
+
 })
