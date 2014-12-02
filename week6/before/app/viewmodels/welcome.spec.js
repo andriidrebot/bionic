6e5acd242d1dc73f2b9620526288f4f34f3d63e3
Andrey Drebot <a.drebot@gmail.com>
2014-12-02 11:30:56 +0200

    Week 6 files were added

---
+++ week6/before/app/viewmodels/welcome.spec.js
@@ -0,0 +1,17 @@
+﻿define(['viewmodels/welcome'], function (ViewModel) {
+
+    describe('viewmodel [welcome]', function () {
+
+        describe('displayName:', function () {
+
+            it('should be define', function () {
+                var viewModel = new ViewModel();
+                expect(viewModel.displayName).toBeDefined();
+            });
+
+        })
+
+    })
+
+
+})
