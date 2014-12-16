c745fe968e62a4e175a8bff4c7418b553aaf52a1
Andrey Drebot <a.drebot@gmail.com>
2014-12-16 19:09:20 +0200

    Gulp minification was added

---
+++ week9/after/gulpfile.js
@@ -0,0 +1,33 @@
+﻿var
+    gulp = require('gulp'),
+
+    del = require('del'),
+    durandal = require('gulp-durandal')
+;
+
+gulp.task('clean', function () {
+    del(['build']);
+});
+
+gulp.task('durandal', ['clean'], function () {
+
+    gulp
+        .src('./index.html')
+        .pipe(gulp.dest('build'))
+
+    gulp
+        .src('./css/**/*.*')
+        .pipe(gulp.dest('build/css'))
+
+    gulp
+        .src('./js/require.js')
+        .pipe(gulp.dest('build/js'))
+
+    return durandal({
+        baseDir: 'app',   //same as default, so not really required.
+        main: 'main.js',  //same as default, so not really required.
+        output: 'main.js', //same as default, so not really required.
+        minify: true
+    }).pipe(gulp.dest('build/app'));
+
+});
